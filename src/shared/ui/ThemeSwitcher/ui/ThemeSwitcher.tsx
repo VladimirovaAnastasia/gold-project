9ff1e4289e886d0438a0afd1de854de7db843ad6
KRASAVA <vladimirova.a-a@yandex.ru>
2023-02-28 21:32:21 +0400

    Add correct svg import

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,8 +2,8 @@ import classNames from "shared/lib/classNames/classNames";
 import cls from './ThemeSwitcher.module.scss';
 import React from "react";
 import {Theme, useTheme} from "app/providers/ThemeProvider";
-import LightIcon from "src/shared/assets/icons/theme-light.svg";
-import DarkIcon from "src/shared/assets/icons/theme-dark.svg";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
 import {Button, ThemeButton} from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
