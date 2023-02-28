5da660761971259886b8e97ad8e00b0237e7a271
KRASAVA <vladimirova.a-a@yandex.ru>
2023-02-28 21:13:06 +0400

    Add Theme Switcher

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classNames from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 
 interface NavBarProps {
@@ -11,6 +12,7 @@ interface NavBarProps {
 export const NavBar = ({className}: NavBarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink to={'/'} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
                     Главная
