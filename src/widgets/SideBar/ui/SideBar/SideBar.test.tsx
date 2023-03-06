d5833816f268305c36750c08278cf382234e4e9f
KRASAVA <vladimirova.a-a@yandex.ru>
2023-03-06 10:16:44 +0400

    Fix name of the component

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { SideBar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { SideBar } from 'widgets/SideBar/ui/SideBar/SideBar';
+import {componentRender} from "shared/lib/tests/componentRender/componentRender";
 
 describe('Sidebar', () => {
     test('with only first param', () => {
